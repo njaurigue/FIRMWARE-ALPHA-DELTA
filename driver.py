30ca301a478e9805886427d00ecb9348c8d4d89b
noahdj37 <noahdj37@gmail.com>
2022-11-20 19:19:02 -0800

    CSS Adjustments

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Chrome
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
